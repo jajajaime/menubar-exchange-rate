7d93e417e077feec21ee969141fb52ef35ab5107
Jaime López <jaimelopezav@gmail.com>
2016-05-23 07:50:06 -0700

    Initial Commit

---
+++ menubar-exchange-rate/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  menubar-exchange-rate
+//
+//  Created by Jaime López on 5/23/16.
+//  Copyright © 2016 Jaime Lopez. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
